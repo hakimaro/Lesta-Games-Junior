b764ac2daa9824afe8b7a05d681259c488784ab7
hakimaro <78315511+hakimaro@users.noreply.github.com>
2022-11-22 15:34:38 +0300

    Add files via upload

---
+++ Ex1/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+bool isEven1(int value) { return value % 2; }
+bool isEven2(int value) { return value & 1; }
+
+using namespace std;
+int main()
+{
+    int value = 4;
+    cout << isEven1(value) << endl;
+    cout << isEven2(value) << endl;
+}
